688159e92e3c05a123c6e8e6f824f5a9628c562b
linjoe2 <linjoe_is@live.nl>
2021-08-05 15:47:59 +0200

    minor updates

--- src/nakama.svelte
+++ src/nakama.svelte
@@ -1,8 +1,8 @@
 <script context="module">
 	import {Client} from "@heroiclabs/nakama-js";
 	
-	export const client = new Client("WeEatChildrenForBreakfast", "192.168.0.133", "7350");
-	//export const client = new Client("WeEatChildrenForBreakfast", "192.168.0.121", "7350");
+	//export const client = new Client("WeEatChildrenForBreakfast", "192.168.0.133", "7350");
+	export const client = new Client("WeEatChildrenForBreakfast", "192.168.0.121", "7350");
 	client.ssl = false;
 	
 	export let session
