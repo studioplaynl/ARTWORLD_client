1d02ca13f21d9d565bb664a1f730bfa33df606f3
maarten <maartenvanderglas@gmail.com>
2021-08-31 21:37:31 +0200

    working updating array online players, and relaying positions 2

--- src/routes/game/scenes/AZC1_Scene.js
+++ src/routes/game/scenes/AZC1_Scene.js
@@ -384,49 +384,36 @@ this.add.graphics()
       this.player.body.setVelocityX(-speed);
 
       // this.arrowDown = true;
-      if (
-        manageSession.updateMovementTimer > manageSession.updateMovementInterval
-      ) {
-        manageSession.sendMoveMessage(Math.round(this.player.x), Math.round(this.player.y));
-        manageSession.updateMovementTimer = 0;
-      }
+      this.sendPlayerMovement();
     } else if (this.cursors.right.isDown) {
       this.player.body.setVelocityX(speed);
       // this.arrowDown = true
-      if (
-        manageSession.updateMovementTimer > manageSession.updateMovementInterval
-      ) {
-        manageSession.sendMoveMessage(Math.round(this.player.x), Math.round(this.player.y));
-        manageSession.updateMovementTimer = 0;
-      }
+      this.sendPlayerMovement();
     }
 
     // Vertical movement
     if (this.cursors.up.isDown) {
       this.player.body.setVelocityY(-speed);
       // this.arrowDown = true
-      if (
-        manageSession.updateMovementTimer > manageSession.updateMovementInterval
-      ) {
-        manageSession.sendMoveMessage(Math.round(this.player.x), Math.round(this.player.y));
-        manageSession.updateMovementTimer = 0;
-      }
+      this.sendPlayerMovement();
     } else if (this.cursors.down.isDown) {
       this.player.body.setVelocityY(speed);
       // this.arrowDown = true
-      if (
-        manageSession.updateMovementTimer > manageSession.updateMovementInterval
-      ) {
-        manageSession.sendMoveMessage(Math.round(this.player.x), Math.round(this.player.y));
-        manageSession.updateMovementTimer = 0;
-      }
+     this.sendPlayerMovement();
     }
 
     // Normalize and scale the velocity so that player can't move faster along a diagonal
     this.player.body.velocity.normalize().scale(speed);
   }
 
-  
+  sendPlayerMovement(){
+    if (
+      manageSession.updateMovementTimer > manageSession.updateMovementInterval
+    ) {
+      manageSession.sendMoveMessage(Math.round(this.player.x), Math.round(this.player.y));
+      manageSession.updateMovementTimer = 0;
+    }
+  }
 
   update(time, delta) {
     //........... PLAYER SHADOW .............................................................................
@@ -485,12 +472,7 @@ this.add.graphics()
         this.player.body.reset(this.target.x, this.target.y);
         this.player.setData("isMovingByClicking", false)
       } else {
-        if (
-          manageSession.updateMovementTimer > manageSession.updateMovementInterval
-        ) {
-          manageSession.sendMoveMessage(this.player.x, this.player.y);
-          manageSession.updateMovementTimer = 0;
-        }
+        this.sendPlayerMovement();
       }
     }
     //....... end MOVE BY CLICKING ......................................................................................
