a7683a5703a891eda179a26fe16203032c2f9aee
maarten <maartenvanderglas@gmail.com>
2021-10-20 13:21:12 +0200

    locations

--- src/nakama.svelte
+++ src/nakama.svelte
@@ -1,17 +1,12 @@
 <script context="module">
   import { Client } from "@heroiclabs/nakama-js";
 
-<<<<<<< HEAD
-=======
-  export const SSL = true;
->>>>>>> 09894ffa2c547f61ecaf6c11b8698cdab252092b
-
   //	export const client = new Client("RHSGvCuxJRmEf347", "192.168.0.133", "7350");
   // export const SSL = false;
 
- // export const client = new Client("RHSGvCuxJRmEf347", "192.168.0.138", "7350", SSL);
+  // export const client = new Client("RHSGvCuxJRmEf347", "192.168.0.138", "7350", SSL);
 
- export const SSL = true;
+  export const SSL = true;
 
   export const client = new Client(
     "RHSGvCuxJRmEf347",
