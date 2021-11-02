ca3f2265de31b0dd63254927cfdc05bfbcf8cac9
atabek-bakh <coderonline@icloud.com>
2021-11-02 08:40:32 +0100

    init location6

---
+++ src/routes/game/scenes/Location6_Scene.js
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+export default class Location6Scene extends Phaser.Scene {
+  constructor() {
+    super("location6_Scene");
+  }
+
+  preload() { }
+
+  async create() {}
+
+  update() { }
+}
