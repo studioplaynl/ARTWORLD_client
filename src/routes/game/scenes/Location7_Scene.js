273a72c9e92b83763953f29ffbbe8d8ee0e7604d
atabek-bakh <coderonline@icloud.com>
2021-11-03 17:27:43 +0100

    zooming added to location7

--- src/routes/game/scenes/Location7_Scene.js
+++ src/routes/game/scenes/Location7_Scene.js
@@ -1,6 +1,8 @@
 import { Scene3D } from "@enable3d/phaser-extension";
 export default class Location7Scene extends Scene3D {
   objects;
+  zooming;
+
   constructor() {
     super("location7_Scene");
   }
@@ -17,10 +19,13 @@ export default class Location7Scene extends Scene3D {
   }
 
   create() {
+    this.scene.stop("UI_Scene");
+
     // this disables 3d ground, blue sky and ability to move around the 3d world with mouse
     this.third.warpSpeed("-ground", "-sky", "-orbitControls");
 
-    this.third.camera.position.set(0, 30, 0);
+    this.zooming = 30;
+    this.third.camera.position.set(0, this.zooming, 0);
     this.third.camera.lookAt(0, 0, 0);
 
     this.third.load
@@ -39,6 +44,39 @@ export default class Location7Scene extends Scene3D {
         height: 1,
       }),
     ];
+
+    //zoom buttons
+
+    let width = this.sys.game.canvas.width;
+    let height = this.sys.game.canvas.height - 60;
+
+    this.zoom = this.add
+      .image(width / 10 + 40, height / 50, "ui_eye")
+      .setOrigin(0)
+      .setDepth(1000)
+      .setScale(width / width / 8);
+
+    this.zoomIn = this.add
+      .image(width / 10 + 120, height / 40, "ui_magnifier_plus")
+      .setOrigin(0)
+      .setDepth(1000)
+      .setScale(width / width / 6)
+      .setInteractive({ useHandCursor: true });
+
+    this.zoomOut = this.add
+      .image(width / 10, height / 40, "ui_magnifier_minus")
+      .setOrigin(0)
+      .setDepth(1000)
+      .setScale(width / width / 6)
+      .setInteractive({ useHandCursor: true });
+
+    this.zoomIn.on("pointerup", () => {
+      this.third.camera.position.set(0, (this.zooming -= 10), 0);
+    });
+
+    this.zoomOut.on("pointerup", () => {
+      this.third.camera.position.set(0, (this.zooming += 10), 0);
+    });
   }
 
   update() {
