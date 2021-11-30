b9417b3297f6f9ee352dadf77f5419026cef8496
atabek-bakh <coderonline@icloud.com>
2021-11-30 18:29:02 +0100

    swiping works on the safari browser

--- src/routes/game/scenes/Location1_Scene.js
+++ src/routes/game/scenes/Location1_Scene.js
@@ -259,17 +259,6 @@ export default class Location1Scene extends Phaser.Scene {
     console.log(this.currentZoom)
 
     this.exampleREXUI()
-
-    // identifies if the pointer is down on a graffiti wall
-    // if the condition is true, the avatar stops any movement
-    this.input.on('pointerdown', (pointer, object) => {
-      if (object[0]?.name && object[0]?.name == "graffitiBrickWall" || object[0]?.name == "graffitiDotWall") {
-        this.graffitiDrawing = true
-      }
-    })
-    this.input.on('pointerup', () => {
-      this.graffitiDrawing = false
-    })
     
   } // end create
 
@@ -1539,6 +1528,19 @@ export default class Location1Scene extends Phaser.Scene {
     }
   }
 
+  identifySurfaceOfSwiping() {
+    // identifies if the pointer is down on a graffiti wall
+    // if the condition is true, the avatar stops any movement
+    this.input.on('pointerdown', (pointer, object) => {
+      if (object[0] && object[0]?.name == "graffitiBrickWall" || object[0]?.name == "graffitiDotWall") {
+        this.graffitiDrawing = true;
+      }
+    })
+    this.input.on('pointerup', () => {
+      this.graffitiDrawing = false
+    })
+  }
+
   playerMovingBySwiping() {
     if (this.input.activePointer.isDown
       && this.isClicking == false &&
@@ -1686,6 +1688,7 @@ export default class Location1Scene extends Phaser.Scene {
 
     //this.playerMovingByClicking()
     this.playerMovingBySwiping()
+    this.identifySurfaceOfSwiping()
 
   } //update
 } //class
