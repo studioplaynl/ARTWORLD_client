7d0a49e895c9daa330a3d5b811bb466148a45373
atabek-bakh <coderonline@icloud.com>
2021-12-07 14:46:28 +0100

    added argument to GraffitiWall class and removedcreateDrawingTexture from location1

--- src/routes/game/class/GraffitiWall.js
+++ src/routes/game/class/GraffitiWall.js
@@ -2,18 +2,14 @@ class GraffitiWall {
   constructor() {
   }
 
-  create(scene, x, y, width, height, name, imageFile = null) {
+  create(scene, x, y, width, height, name, color, imageFile = null) {
     const rt = scene.add.renderTexture(x, y, width, height).setInteractive().setDepth(1001).setName(name)
-    console.log("RT", rt)
+    // checking if a drawing wall has a front image
     if (imageFile) {
       const graffitiWall = scene.add.image(x, y, imageFile).setOrigin(0).setDepth(1000)
       graffitiWall.displayWidth = width
       graffitiWall.displayHeight = height
     }
-
-    let hsv = Phaser.Display.Color.HSVColorWheel();
-
-    let i = 0;
 
     scene.input.keyboard.on('keydown-' + 'SPACE', function () {
       rt.clear();
@@ -22,15 +18,14 @@ class GraffitiWall {
     rt.on('pointerdown', function (pointer) {
       this.graffitiDrawing = true
       this.isClicking = true
-      this.draw('brush', pointer.worldX - x, pointer.worldY - y, 1, hsv[i].color);
+      this.draw('brush', pointer.worldX - x, pointer.worldY - y, 1, color);
     });
 
     rt.on('pointermove', function (pointer) {
       if (pointer.isDown) {
         this.graffitiDrawing = true
         this.isClicking = true
-        this.draw('brush', pointer.worldX - x, pointer.worldY - y, 1, hsv[i].color);
-        i = Phaser.Math.Wrap(i + 1, 0, 360);
+        this.draw('brush', pointer.worldX - x, pointer.worldY - y, 1, color);
       }
     })
 
