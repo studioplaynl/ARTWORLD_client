f7d5d1e13f5cea890b0b607064d9664491f7a4df
atabek-bakh <coderonline@icloud.com>
2022-01-28 16:19:06 +0100

    class ScrollablePanel is created

--- src/routes/game/class/Player.js
+++ src/routes/game/class/Player.js
@@ -3,6 +3,7 @@ import CoordinatesTranslator from "./CoordinatesTranslator";
 import { listObjects, listImages, convertImage, getFullAccount } from "../../../api.js";
 import HistoryTracker from "./HistoryTracker"
 import ArtworkList from "./ArtworkList";
+import ScrollablePanel from "./ScrollablePanel";
 
 class Player {
   constructor() { }
@@ -204,7 +205,7 @@ class Player {
         background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 10, 0xffffff),
 
         panel: {
-          child: this.createPanel(scene, scene.playerLikedPanelKeys),
+          child: ScrollablePanel.createPanel(scene, scene.playerLikedPanelKeys),
         },
 
         slider: {
@@ -255,7 +256,7 @@ class Player {
           background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 10, 0xffffff),
 
           panel: {
-            child: this.createPanel(scene, scene.playerLikedPanelKeys),
+            child: ScrollablePanel.createPanel(scene, scene.playerLikedPanelKeys),
           },
 
           slider: {
@@ -325,73 +326,6 @@ class Player {
         scene.isPlayerItemsBarDisplayed = false
       }
     });
-  }
-
-  createPanel(scene, data) {
-    var sizer = scene.rexUI.add.sizer({
-      orientation: 'y',
-      // space: { item: 10 }
-    })
-      .add(
-        this.createTable(scene, data, 'artworks', 1), // child
-        { expand: true }
-      )
-    return sizer;
-  }
-
-  createTable(scene, data, key, columns) {
-    var items = data[key];
-    var rows = Math.ceil(items.length / columns);
-    var table = scene.rexUI.add.gridSizer({
-      column: columns,
-      row: rows,
-
-      columnProportions: 1,
-      space: { column: 10, row: 10 },
-      name: key  // Search this name to get table back
-    });
-
-    var item, r, c;
-    var iconSize = (columns === 1) ? 80 : 40;
-    for (var i = 0; i < items.length; i++) {
-      item = items[i];
-      c = i % columns;
-      r = (i - c) / columns;
-      table.add(
-        this.createIcon(scene, item, iconSize, iconSize),
-        c,
-        r,
-        'top',
-        5, // distance between artworks
-        true
-      );
-    }
-
-    return scene.rexUI.add.sizer({
-      orientation: 'y',
-      space: { left: 10, right: 10, top: 10, bottom: 10, item: 10 }
-    })
-      .addBackground(
-        scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(4, 0x000000, 1)
-      )
-      .add(table, // child
-        1, // proportion
-        'center', // align
-        0, // paddingConfig
-        true // expand
-      );
-  }
-
-  createIcon(scene, item) {
-    var label = scene.rexUI.add.label({
-      orientation: 'y',
-      // icon: scene.rexUI.add.roundRectangle(0, 0, iconWidth, iconHeight, 5, COLOR_LIGHT),
-      icon: scene.add.image(0, 0, item.name),
-
-      // space: { icon: 3 }
-    });
-
-    return label;
   }
 
   moveByCursor(scene) {
@@ -966,7 +900,7 @@ class Player {
             background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 10, 0xffffff),
 
             panel: {
-              child: this.createPanel(scene, scene.onlinePlayerDownloadedImages),
+              child: ScrollablePanel.createPanel(scene, scene.onlinePlayerDownloadedImages),
             },
 
             slider: {
---
+++ src/routes/game/class/ScrollablePanel.js
@@ -0,0 +1,141 @@
+class ScrollablePanel {
+
+  constructor() { }
+
+  createPanel(scene, data) {
+    var sizer = scene.rexUI.add.sizer({
+      orientation: 'y',
+      // space: { item: 10 }
+    })
+      .add(
+        this.createTable(scene, data, 'artworks', 1), // child
+        { expand: true }
+      )
+    return sizer;
+  }
+
+  createTable(scene, data, key, columns) {
+    var items = data[key];
+    var rows = Math.ceil(items.length / columns);
+    var table = scene.rexUI.add.gridSizer({
+      column: columns,
+      row: rows,
+
+      columnProportions: 1,
+      space: { column: 10, row: 10 },
+      name: key  // Search this name to get table back
+    });
+
+    var item, r, c;
+    var iconSize = (columns === 1) ? 80 : 40;
+    for (var i = 0; i < items.length; i++) {
+      item = items[i];
+      c = i % columns;
+      r = (i - c) / columns;
+      table.add(
+        this.createIcon(scene, item, iconSize, iconSize),
+        c,
+        r,
+        'top',
+        5, // distance between artworks
+        true
+      );
+    }
+
+    return scene.rexUI.add.sizer({
+      orientation: 'y',
+      space: { left: 10, right: 10, top: 10, bottom: 10, item: 10 }
+    })
+      .addBackground(
+        scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(4, 0x000000, 1)
+      )
+      .add(table, // child
+        1, // proportion
+        'center', // align
+        0, // paddingConfig
+        true // expand
+      );
+  }
+
+  createIcon(scene, item) {
+    var label = scene.rexUI.add.label({
+      orientation: 'y',
+      // icon: scene.rexUI.add.roundRectangle(0, 0, iconWidth, iconHeight, 5, COLOR_LIGHT),
+      icon: scene.add.image(0, 0, item.name),
+
+      // space: { icon: 3 }
+    });
+
+    return label;
+  }
+  createPanel(scene, data) {
+    var sizer = scene.rexUI.add.sizer({
+      orientation: 'y',
+      // space: { item: 10 }
+    })
+      .add(
+        this.createTable(scene, data, 'artworks', 1), // child
+        { expand: true }
+      )
+    return sizer;
+  }
+
+  createTable(scene, data, key, columns) {
+    var items = data[key];
+    var rows = Math.ceil(items.length / columns);
+    var table = scene.rexUI.add.gridSizer({
+      column: columns,
+      row: rows,
+
+      columnProportions: 1,
+      space: { column: 10, row: 10 },
+      name: key  // Search this name to get table back
+    });
+
+    var item, r, c;
+    var iconSize = (columns === 1) ? 80 : 40;
+    for (var i = 0; i < items.length; i++) {
+      item = items[i];
+      c = i % columns;
+      r = (i - c) / columns;
+      table.add(
+        this.createIcon(scene, item, iconSize, iconSize),
+        c,
+        r,
+        'top',
+        5, // distance between artworks
+        true
+      );
+    }
+
+    return scene.rexUI.add.sizer({
+      orientation: 'y',
+      space: { left: 10, right: 10, top: 10, bottom: 10, item: 10 }
+    })
+      .addBackground(
+        scene.rexUI.add.roundRectangle(0, 0, 0, 0, 0, undefined).setStrokeStyle(4, 0x000000, 1)
+      )
+      .add(table, // child
+        1, // proportion
+        'center', // align
+        0, // paddingConfig
+        true // expand
+      );
+  }
+
+  createIcon(scene, item) {
+    var label = scene.rexUI.add.label({
+      orientation: 'y',
+      // icon: scene.rexUI.add.roundRectangle(0, 0, iconWidth, iconHeight, 5, COLOR_LIGHT),
+      icon: scene.add.image(0, 0, item.name),
+
+      // space: { icon: 3 }
+    });
+
+    return label;
+  }
+
+}
+
+
+export default new ScrollablePanel()
