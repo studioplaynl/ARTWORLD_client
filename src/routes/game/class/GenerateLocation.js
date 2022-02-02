09377f2d8139c58a72e4bbbe411cfcb42c298a71
atabek-bakh <coderonline@icloud.com>
2022-02-02 12:54:53 +0100

    bug fix

--- src/routes/game/class/GenerateLocation.js
+++ src/routes/game/class/GenerateLocation.js
@@ -103,7 +103,7 @@ export default class GenerateLocation extends Phaser.GameObjects.Container {
 
         // this.enterButtonHitArea.displayWidth = width / 1.05
 
-        this.enterCircle = this.scene.add.circle(this.x, enterButtonY + 5, 30, 0xFFF2CC).setOrigin(0.5, 0.5).setVisible(false)
+        this.enterCircle = this.scene.add.circle(this.x, enterButtonY + 5, 30, 0xffffff).setOrigin(0.5, 0.5).setVisible(false).setStrokeStyle(4, 0x000000)
         this.enterButton = this.scene.add.image(this.x, enterButtonY, this.enterButtonImage).setOrigin(0.5, 0.5).setDepth(200).setVisible(false)
 
 
