2e2b783b877ec719e81db6da8dfa8c81e1a6f9eb
atabek-bakh <coderonline@icloud.com>
2022-02-11 12:15:51 +0100

    class for server call

---
+++ src/routes/game/class/ServerCall.js
@@ -0,0 +1,72 @@
+import ManageSession from "../ManageSession"
+import { getAccount, updateObject, listObjects } from '../../../api.js'
+
+class ServerCall {
+  constructor() { }
+  async getServerObject(collection, userID, maxItems) {
+    Promise.all([listObjects(collection, userID, maxItems)])
+      .then(response => {
+        console.log("collection", collection)
+        console.log("response", response)
+
+        //check if the object exists
+        if (response[0].length > 0) {
+          //the object exists: addressbook
+
+          // check if the right object exists: addressbook_user_id
+          let filteredResponse = response[0].filter(element => {
+            //console.log(collection + "_" + ManageSession.userProfile.id, typeof collection)
+            console.log("element", element)
+            return element.key == collection + "_" + ManageSession.userProfile.id
+          }
+          )
+          console.log("filteredResponse", filteredResponse)
+
+          if (filteredResponse.length > 0) {
+            //the right collection object exists, but check if there is data in de object, in the expected format
+
+            if (typeof filteredResponse[0].value[collection] != "undefined") {
+              //the object is in the right format (object.value.object), we assign our local copy
+              ManageSession[collection] = filteredResponse[0].value
+              console.log("ManageSession." + collection, ManageSession[collection])
+              return
+            } else {
+              //when the right addressbook does not exist: make an empty one
+              //addressbook_userid.value exists but .addressbook  
+              this.createEmptyServerObject(collection)
+            }
+
+          } else {
+            //when the right addressbook does not exist: make an empty one
+            this.createEmptyServerObject(collection)
+
+          }
+          console.log("ManageSession." + collection, ManageSession[collection])
+
+        } else {
+          //the addressbook does not exist: make an empty one
+          this.createEmptyServerObject(collection)
+        }
+      })
+  }
+
+  async createEmptyServerObject(collection) {
+    //general method of creating an array inside an object with the argument of the method
+    console.log("createEmptyServerObject")
+    console.log(collection)
+
+    ManageSession[collection] = { [collection]: [] }
+
+    const type = collection
+    const name = type + "_" + ManageSession.userProfile.id
+    const pub = 2
+    const value = ManageSession[collection]
+    console.log(" ManageSession. empty", ManageSession[collection])
+    updateObject(type, name, value, pub)
+  }
+
+
+
+}
+
+export default new ServerCall()
--- src/routes/game/scenes/NetworkBoot.js
+++ src/routes/game/scenes/NetworkBoot.js
@@ -1,5 +1,5 @@
 import ManageSession from "../ManageSession"
-import { getAccount, updateObject, listObjects } from '../../../api.js'
+import ServerCall from "../class/ServerCall"
 
 export default class NetworkBoot extends Phaser.Scene {
   constructor() {
@@ -13,77 +13,10 @@ export default class NetworkBoot extends Phaser.Scene {
 
     //we launch the player last location when we have a socket with the server
     await ManageSession.createSocket()
-      .then(rec => {
-        //console.log(ManageSession.launchLocation)
-
-        this.getServerObject("liked", ManageSession.userProfile.id, 10)
-        //this.getAddressBook()
-        this.getServerObject("addressbook", ManageSession.userProfile.id, 10)
+      .then(() => {
+        ServerCall.getServerObject("liked", ManageSession.userProfile.id, 10)
+        ServerCall.getServerObject("addressbook", ManageSession.userProfile.id, 10)
         this.scene.launch(ManageSession.launchLocation)
       })
   }
-
-  async getServerObject(collection, userID, maxItems) {
-    Promise.all([listObjects(collection, userID, maxItems)])
-      .then(response => {
-        console.log("collection", collection)
-        console.log("response", response)
-
-        //check if the object exists
-        if (response[0].length > 0) {
-          //the object exists: addressbook
-
-          // check if the right object exists: addressbook_user_id
-          let filteredResponse = response[0].filter(element => {
-            //console.log(collection + "_" + ManageSession.userProfile.id, typeof collection)
-            console.log("element", element)
-            return element.key == collection + "_" + ManageSession.userProfile.id
-          }
-          )
-          console.log("filteredResponse", filteredResponse)
-
-          if (filteredResponse.length > 0) {
-            //the right collection object exists, but check if there is data in de object, in the expected format
-
-            if (typeof filteredResponse[0].value[collection] != "undefined") {
-              //the object is in the right format (object.value.object), we assign our local copy
-              ManageSession[collection] = filteredResponse[0].value
-              console.log("ManageSession." + collection, ManageSession[collection])
-              return
-            } else {
-              //when the right addressbook does not exist: make an empty one
-              //addressbook_userid.value exists but .addressbook  
-              this.createEmptyServerObject(collection)
-            }
-
-          } else {
-            //when the right addressbook does not exist: make an empty one
-            this.createEmptyServerObject(collection)
-
-          }
-          console.log("ManageSession." + collection, ManageSession[collection])
-
-        } else {
-          //the addressbook does not exist: make an empty one
-          this.createEmptyServerObject(collection)
-        }
-      })
-  }
-
-  async createEmptyServerObject(collection) {
-    //general method of creating an array inside an object with the argument of the method
-    console.log("createEmptyServerObject")
-    console.log(collection)
-
-    ManageSession[collection] = { [collection]:[] }
-
-    const type = collection
-    const name = type + "_" + ManageSession.userProfile.id
-    const pub = 2
-    const value = ManageSession[collection]
-    console.log(" ManageSession. empty", ManageSession[collection])
-    updateObject(type, name, value, pub)
-  }
-
-  
 }
