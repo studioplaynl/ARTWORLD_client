18ce113d8abe6e7f6b44873fea7981da7202fe85
linjoe2 <ik@Lindseyschaap.nl>
2022-03-11 13:35:30 +0100

    start for otheruser itemsbar

--- src/nakama.svelte
+++ src/nakama.svelte
@@ -7,12 +7,12 @@
 
   // export const client = new Client("RHSGvCuxJRmEf347", "192.168.0.138", "7350", SSL);
 
-  export const SSL = true;
+  export const SSL = false;
 
   export const client = new Client(
     "RHSGvCuxJRmEf347",
-    "betanakama.toekomst.school",
-    "",
+    "artworld.vrolijkheid.nl",
+    "7350",
     SSL
   );
 </script>
