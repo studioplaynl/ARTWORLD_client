67ccaf8d884b7d1a7d665fbec906415b04381092
linjoe2 <linjoe_is@live.nl>
2022-03-25 13:50:26 +0100

    tutorial sequence working

--- src/routes/tutorials/firstTime.svelte
+++ src/routes/tutorials/firstTime.svelte
@@ -1,20 +1,36 @@
 <script>
     import Tap from "./gestures/tap.svelte"
     import Swipe from "./gestures/swipe.svelte"
-
-    let hide = true
+    import {onMount, beforeUpdate} from "svelte"
+    let current = 0
+    let hide = []
+    hide[0] = false
+    let sequence = [
+        {type: "swipe", direction: "left", element: "psw", delay: 2000},
+        {type: "tap", doubleTap: true, element: "email", delay: 1000},
+        {type: "tap", element: "itemsButton", delay: 5000},
+    ]
     
-    setTimeout(()=>{
-        hide= false
-    },7000)
- 
-    let done = false
-
+    beforeUpdate(() => {
+        if(hide[current]) {
+            current++
+            hide[current] = false
+        }
+    });
 
 </script>
 
-{#if !hide && !done}
-<Tap element = "itemsButton" bind:done={done}/>
-{/if}
+    {#each sequence as seq, i}
+            {#if seq.type == "tap" }
+                {#if !hide[i]}
+                    <Tap num={i} element = {seq.element} doubleTap={seq.doubleTap} delay={seq.delay} bind:hide={hide[i]}/>
+                {/if}
+            {/if}
+            {#if seq.type == "swipe" }
+                {#if !hide[i]}
+                    <Swipe num={i} element = {seq.element} direction = {seq.direction} delay={seq.delay} bind:hide={hide[i]} />
+                {/if}
+            {/if}
+    {/each}
+ 
 
-<!-- <Swipe element = "itemsButton" bind:done={done} /> -->
