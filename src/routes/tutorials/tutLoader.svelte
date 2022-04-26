241808d492954b24f2a931eea59b82bd49da9950
maarten van der glas <maartenvanderglas@gmail.com>
2022-04-26 15:24:02 +0200

    small code clean up

--- src/routes/tutorials/tutLoader.svelte
+++ src/routes/tutorials/tutLoader.svelte
@@ -1,78 +1,95 @@
 <script>
-    import Tap from "./gestures/tap.svelte"
-    import Swipe from "./gestures/swipe.svelte"
-    import { onMount} from "svelte"
-    import {tutorial, CurrentApp} from "../../session"
-    import ManageSession from "../game/ManageSession"
-    import {Achievements} from "../../storage"
-    let current = 0
-    let hide = []
-    let sequence = []
-    hide[0] = false
+    import Tap from "./gestures/tap.svelte";
+    import Swipe from "./gestures/swipe.svelte";
+    import { onMount } from "svelte";
+    import { tutorial, CurrentApp } from "../../session";
+    import ManageSession from "../game/ManageSession";
+    import { Achievements } from "../../storage";
+    let current = 0;
+    let hide = [];
+    let sequence = [];
+    hide[0] = false;
 
     tutorial.subscribe((value) => {
-    if (!!value) {
-        sequence = value 
-    }
-})
+        if (!!value) {
+            sequence = value;
+        }
+    });
 
-
-
-    
     onMount(() => {
-        console.log("achievements")
-        console.log(Achievements.get())
-        document.body.addEventListener('click', ()=>{
-            if(hide[current] && hide.length > current) {
-                current++
-                hide[current] = false
-                if(sequence[current].type == "achievement" ) {
-                    Achievements.create(sequence[current].name, "{}")
+        console.log("achievements");
+        console.log(Achievements.get());
+        document.body.addEventListener("click", () => {
+            if (hide[current] && hide.length > current) {
+                current++;
+                hide[current] = false;
+                if (sequence[current].type == "achievement") {
+                    Achievements.create(sequence[current].name, "{}");
                 }
             }
         });
 
-
-        
-    CurrentApp.subscribe((value) => {
+        CurrentApp.subscribe((value) => {
             if (value == "game") {
-
-                setTimeout(()=>{
-                    if(!Achievements.find("firstLogin")){
-                        Achievements.create("firstLogin", "{}")
+                setTimeout(() => {
+                    if (!Achievements.find("firstLogin")) {
+                        Achievements.create("firstLogin", "{}");
                     }
-                },1500)
+                }, 1500);
 
-                setTimeout(()=>{
-
-                    if(!!!Achievements.find("onboardMove")){
-                            $tutorial = [
-                                {type: "swipe", direction: "right", element: "phaserId", posX: window.innerWidth/2 , posY: window.innerHeight/2-100, delay: 500},
-                                {type: "tap", doubleTap: true, element: "phaserId", posX: window.innerWidth/2-150 , posY: window.innerHeight/2-200, delay: 1000},
-                                {type: "achievement", name: "onboardMove"}
-                            ]
-                        }
-                },4000)
+                setTimeout(() => {
+                    if (!!!Achievements.find("onboardMove")) {
+                        $tutorial = [
+                            {
+                                type: "swipe",
+                                direction: "right",
+                                element: "phaserId",
+                                posX: window.innerWidth / 2,
+                                posY: window.innerHeight / 2 - 100,
+                                delay: 500,
+                            },
+                            {
+                                type: "tap",
+                                doubleTap: true,
+                                element: "phaserId",
+                                posX: window.innerWidth / 2 - 150,
+                                posY: window.innerHeight / 2 - 200,
+                                delay: 1000,
+                            },
+                            { type: "achievement", name: "onboardMove" },
+                        ];
+                    }
+                }, 4000);
             }
-    })
-
+        });
     });
-
- 
-
 </script>
 
-    {#each sequence as seq, i}
-            {#if seq.type == "tap" }
-                {#if !hide[i]}
-                    <Tap num={i} element = {seq.element} doubleTap={seq.doubleTap} posY={seq.posY} posX={seq.posX} delay={seq.delay} bind:hide={hide[i]}/>
-                {/if}
-            {/if}
-            {#if seq.type == "swipe" }
-                {#if !hide[i]}
-                    <Swipe num={i} element = {seq.element} direction = {seq.direction} posY={seq.posY} posX={seq.posX} delay={seq.delay} bind:hide={hide[i]} />
-                {/if}
-            {/if}
-    {/each}
- 
-
+{#each sequence as seq, i}
+    {#if seq.type == "tap"}
+        {#if !hide[i]}
+            <Tap
+                num={i}
+                element={seq.element}
+                doubleTap={seq.doubleTap}
+                posY={seq.posY}
+                posX={seq.posX}
+                delay={seq.delay}
+                bind:hide={hide[i]}
+            />
+        {/if}
+    {/if}
+    {#if seq.type == "swipe"}
+        {#if !hide[i]}
+            <Swipe
+                num={i}
+                element={seq.element}
+                direction={seq.direction}
+                posY={seq.posY}
+                posX={seq.posX}
+                delay={seq.delay}
+                bind:hide={hide[i]}
+            />
+        {/if}
+    {/if}
+{/each}
--- src/storage.js
+++ src/storage.js
@@ -1,5 +1,5 @@
-import { Session,Profile, Error,Succes} from "./session.js"
-import { deleteObject, updateObject, listAllObjects} from "./api"
+import { Session, Profile, Error, Succes } from "./session.js"
+import { deleteObject, updateObject, listAllObjects } from "./api"
 import ManageSession from "./routes/game/ManageSession.js"; //push awards to ManageSession
 import { get } from 'svelte/store'
 import { writable } from 'svelte/store';
@@ -7,65 +7,65 @@ import { writable } from 'svelte/store';
 
 
 
-function createAchievement(){
-    const { subscribe, set, update } = writable([]);
+function createAchievement() {
+  const { subscribe, set, update } = writable([]);
 
-    return {
-		subscribe,
-        set,
-        update,
-		create: (key, value) => {
-                let ach = get(Achievements)   
-                if( !!ach.find(element => element.key == key)) return
-                updateObject("achievements", key, value, true).then( (value) =>{
-                let obj = {key, value}
-                ach.push(obj)
-                console.log("ach")
-                console.log(ach)
-                console.log(key)
-                Achievements.set(ach)
-                return ach
-                })
-            },
-        get: () => {
-            let ach = get(Achievements)
-            let Sess = get(Session)
-            if(!!ach && ach.length > 0) return ach
-            else {
-            if(!!Sess)listAllObjects("achievements", Sess.user_id).then((ach)=>{
-                Achievements.set(ach)
-                return ach
-            })
-            }
-        },
-        find: (key) => {
-                  let ach = get(Achievements)
-                  let i = ach.findIndex((element) => element.key == key)
-                  if(i != -1) return ach[i].value
-                  else return undefined
-                },
-        // update: (Item) =>{
-        //             updateObject("achievements", Item.name, Item.value, Item.pub).then( (value) =>{
-        //                 let ach = get(achievements)
-        //                 const itemNum = ach.findIndex((element) => element.name == Item.name)
-        //                 ach[itemNum] = Item
-        //                 Achievements.set(ach)
-        //                 return ach
-        //             })
-                    
-        //         },
-            
-        delete: (key) =>{
-            Achievements.update((value)=>{
-                const itemNum = value.findIndex((element) => element.key == key)
-                if(itemNum == -1) return value
-                value.splice(itemNum, 1);
-                deleteObject("achievements", key)
-                return value
-            })
-        },
+  return {
+    subscribe,
+    set,
+    update,
+    create: (key, value) => {
+      let ach = get(Achievements)
+      if (!!ach.find(element => element.key == key)) return
+      updateObject("achievements", key, value, true).then((value) => {
+        let obj = { key, value }
+        ach.push(obj)
+        console.log("ach")
+        console.log(ach)
+        console.log(key)
+        Achievements.set(ach)
+        return ach
+      })
+    },
+    get: () => {
+      let ach = get(Achievements)
+      let Sess = get(Session)
+      if (!!ach && ach.length > 0) return ach
+      else {
+        if (!!Sess) listAllObjects("achievements", Sess.user_id).then((ach) => {
+          Achievements.set(ach)
+          return ach
+        })
+      }
+    },
+    find: (key) => {
+      let ach = get(Achievements)
+      let i = ach.findIndex((element) => element.key == key)
+      if (i != -1) return ach[i].value
+      else return undefined
+    },
+    // update: (Item) =>{
+    //             updateObject("achievements", Item.name, Item.value, Item.pub).then( (value) =>{
+    //                 let ach = get(achievements)
+    //                 const itemNum = ach.findIndex((element) => element.name == Item.name)
+    //                 ach[itemNum] = Item
+    //                 Achievements.set(ach)
+    //                 return ach
+    //             })
 
-	};
+    //         },
+
+    delete: (key) => {
+      Achievements.update((value) => {
+        const itemNum = value.findIndex((element) => element.key == key)
+        if (itemNum == -1) return value
+        value.splice(itemNum, 1);
+        deleteObject("achievements", key)
+        return value
+      })
+    },
+
+  };
 }
 
 
@@ -93,7 +93,7 @@ export const Achievements = createAchievement()
 //             achievements.set(ach)
 //             return ach
 //         })
-        
+
 //     },
 
 //     delete: (key) =>{
@@ -129,106 +129,106 @@ export const Achievements = createAchievement()
 
 export const Likes = {
 
-  create: (key, value) =>{
+  create: (key, value) => {
     let ach = get(achievements)
-    if( !!ach.find(element => element.key == key)) return
-      updateObject("achievements", key, value, true).then( (value) =>{
-        let obj = {key, value}
-       achievements.update((v) => {v.push(obj); return v} )
-       // return true
+    if (!!ach.find(element => element.key == key)) return
+    updateObject("achievements", key, value, true).then((value) => {
+      let obj = { key, value }
+      achievements.update((v) => { v.push(obj); return v })
+      // return true
     })
 
   },
 
-  update: (Item) =>{
-      updateObject("achievements", Item.name, Item.value, Item.pub).then( (value) =>{
-          let ach = get(achievements)
-          const itemNum = ach.findIndex((element) => element.name == Item.name)
-          ach[itemNum] = Item
-          achievements.set(ach)
-          return ach
-      })
-      
+  update: (Item) => {
+    updateObject("achievements", Item.name, Item.value, Item.pub).then((value) => {
+      let ach = get(achievements)
+      const itemNum = ach.findIndex((element) => element.name == Item.name)
+      ach[itemNum] = Item
+      achievements.set(ach)
+      return ach
+    })
+
   },
 
-  delete: (key) =>{
-      achievements.update((value)=>{
-        const itemNum = value.findIndex((element) => element.key == key)
-        if(itemNum == -1) return value
-        value.splice(itemNum, 1);
-        deleteObject("achievements", key)
-        return value
-      })
+  delete: (key) => {
+    achievements.update((value) => {
+      const itemNum = value.findIndex((element) => element.key == key)
+      if (itemNum == -1) return value
+      value.splice(itemNum, 1);
+      deleteObject("achievements", key)
+      return value
+    })
   },
   find: (key) => {
     let ach = get(achievements)
     let i = ach.findIndex((element) => element.key == key)
-    if(i != -1) return ach[i].value
+    if (i != -1) return ach[i].value
     else return undefined
   },
 
-  get: () =>{
-      let ach = get(achievements)
-      if(!!ach && ach.length > 0) return ach
-      else {
-        listAllObjects("achievements", Sess.user_id).then((ach)=>{
-          achievements.set(ach)
-          return ach
-        })
-      }
+  get: () => {
+    let ach = get(achievements)
+    if (!!ach && ach.length > 0) return ach
+    else {
+      listAllObjects("achievements", Sess.user_id).then((ach) => {
+        achievements.set(ach)
+        return ach
+      })
+    }
   }
 
 }
 
 export const addressbook = {
 
-    create: (key, value) =>{
+  create: (key, value) => {
+    let ach = get(achievements)
+    if (!!ach.find(element => element.key == key)) return
+    updateObject("achievements", key, value, true).then((value) => {
+      let obj = { key, value }
+      achievements.update((v) => { v.push(obj); return v })
+      // return true
+    })
+
+  },
+
+  update: (Item) => {
+    updateObject("achievements", Item.name, Item.value, Item.pub).then((value) => {
       let ach = get(achievements)
-      if( !!ach.find(element => element.key == key)) return
-        updateObject("achievements", key, value, true).then( (value) =>{
-          let obj = {key, value}
-         achievements.update((v) => {v.push(obj); return v} )
-         // return true
+      const itemNum = ach.findIndex((element) => element.name == Item.name)
+      ach[itemNum] = Item
+      achievements.set(ach)
+      return ach
+    })
+
+  },
+
+  delete: (key) => {
+    achievements.update((value) => {
+      const itemNum = value.findIndex((element) => element.key == key)
+      if (itemNum == -1) return value
+      value.splice(itemNum, 1);
+      deleteObject("achievements", key)
+      return value
+    })
+  },
+  find: (key) => {
+    let ach = get(achievements)
+    let i = ach.findIndex((element) => element.key == key)
+    if (i != -1) return ach[i].value
+    else return undefined
+  },
+
+  get: () => {
+    let ach = get(achievements)
+    if (!!ach && ach.length > 0) return ach
+    else {
+      listAllObjects("achievements", Sess.user_id).then((ach) => {
+        achievements.set(ach)
+        return ach
       })
-
-    },
-
-    update: (Item) =>{
-        updateObject("achievements", Item.name, Item.value, Item.pub).then( (value) =>{
-            let ach = get(achievements)
-            const itemNum = ach.findIndex((element) => element.name == Item.name)
-            ach[itemNum] = Item
-            achievements.set(ach)
-            return ach
-        })
-        
-    },
-
-    delete: (key) =>{
-        achievements.update((value)=>{
-          const itemNum = value.findIndex((element) => element.key == key)
-          if(itemNum == -1) return value
-          value.splice(itemNum, 1);
-          deleteObject("achievements", key)
-          return value
-        })
-    },
-    find: (key) => {
-      let ach = get(achievements)
-      let i = ach.findIndex((element) => element.key == key)
-      if(i != -1) return ach[i].value
-      else return undefined
-    },
-
-    get: () =>{
-        let ach = get(achievements)
-        if(!!ach && ach.length > 0) return ach
-        else {
-          listAllObjects("achievements", Sess.user_id).then((ach)=>{
-            achievements.set(ach)
-            return ach
-          })
-        }
     }
+  }
 
 }
