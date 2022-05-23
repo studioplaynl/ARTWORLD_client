d6dcd1ee04fe1bb94f4182cef9e5ee195ff3a7b3
atabek-bakh <coderonline@icloud.com>
2022-05-23 17:12:09 +0200

    receiving object on canvas

--- src/routes/apps/drawingChallenge.svelte
+++ src/routes/apps/drawingChallenge.svelte
@@ -5,45 +5,72 @@
 
   let canvas;
   let canv;
-  let posX, posY;
-  let isDrawing = false;
+  // let posX, posY;
+  // let isDrawing = false;
+  // let receivedLayers;
 
   onMount(async () => {
     fabric.Object.prototype.transparentCorners = false;
 
-    console.log("managesession.socket", ManageSession.socket);
+    // console.log("managesession.socket", ManageSession.socket);
 
     ManageSession.socket.onstreamdata = (streamdata) => {
       let data = JSON.parse(streamdata.data);
       console.log("Receiving data from stream", data);
+
+      fabric.loadSVGFromString(data.action, function (objects, options) {
+        objects.forEach(function (svg) {
+          svg.set({
+            top: 90,
+            left: 90,
+            originX: "center",
+            originY: "center",
+          });
+          svg.scaleToWidth(50);
+          svg.scaleToHeight(50);
+          canvas.add(svg).renderAll();
+        });
+      });
     };
 
     canvas = new fabric.Canvas(canv, {
       isDrawingMode: true,
     });
 
-    canvas.on("mouse:down", () => {
-      isDrawing = true;
-    });
+    // canvas.on("mouse:down", () => {
+    //   isDrawing = true;
+    // });
 
-    canvas.on("mouse:move", (options) => {
-      // start capturing the data to send only after pointer is down
-      if (isDrawing) {
-        console.log("Started drawing!");
+    // canvas.on("mouse:move", (options) => {
+    //   // // start capturing the data to send only after pointer is down
+    //   // if (isDrawing) {
+    //   //   console.log("Started drawing!");
 
-        // all data to send
-        posX = options.e.layerX;
-        posY = options.e.layerY;
-        const location = "drawingchallenge";
-        const data = `{ "posX": ${posX}, "posY": ${posY}, "location": "${location}" }`;
+    //   //   // // all data to send
+    //   //   // posX = options.e.layerX;
+    //   //   // posY = options.e.layerY;
+    //   //   // const location = "drawingchallenge";
+    //   //   // const data = `{ "posX": ${posX}, "posY": ${posY}, "location": "${location}" }`;
 
-        // send data
-        ManageSession.socket.rpc("move_position", data);
-      }
-    });
+    //   //   // // send data
+    //   //   // ManageSession.socket.rpc("move_position", data);
+    //   // }
+    // });
 
     canvas.on("mouse:up", () => {
-      isDrawing = false;
+      // console.log("canvas.toJSON()", canvas.toJSON());
+      const canvasData = JSON.stringify(canvas.toSVG());
+      // console.log("canvasData", canvasData);
+
+      // all data to send
+
+      const location = "drawingchallenge";
+      const dataToSend = `{ "action": ${canvasData}, "location": "${location}" }`;
+      console.log("dataToSend", dataToSend);
+      // send data
+      ManageSession.socket.rpc("move_position", dataToSend);
+
+      // isDrawing = false;
     });
   });
 </script>
