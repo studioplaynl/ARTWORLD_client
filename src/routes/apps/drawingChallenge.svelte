2b6000a0be307e6b9c2991355e98072c6f77e042
atabek-bakh <coderonline@icloud.com>
2022-05-24 12:41:45 +0200

    code clean up and comments drawingChallenge

--- src/routes/apps/drawingChallenge.svelte
+++ src/routes/apps/drawingChallenge.svelte
@@ -11,29 +11,20 @@
   // let receivedLayers;
 
   onMount(async () => {
+    // canvas settings
     fabric.Object.prototype.transparentCorners = false;
+    canvas = new fabric.Canvas(canv, {
+      isDrawingMode: true,
+    });
 
-    // console.log("managesession.socket", ManageSession.socket);
-
+    // listening to the stream to get actions of other person's drawing
     ManageSession.socket.onstreamdata = (streamdata) => {
       let data = JSON.parse(streamdata.data);
 
-      // console.log("$Session.user_id", $Session.user_id);
-      // console.log("data.user_id", data.user_id);
-
-      console.log("data received", data.action);
-
       if ($Session.user_id != data.user_id) {
+        // apply drawings to the canvas if only it is received from other participant
         fabric.loadSVGFromString(data.action, function (objects, options) {
           objects.forEach(function (svg) {
-            // svg.set({
-            //   top: 90,
-            //   left: 90,
-            //   originX: "center",
-            //   originY: "center",
-            // });
-            // svg.scaleToWidth(50);
-            // svg.scaleToHeight(50);
             canvas.add(svg).renderAll();
           });
         });
@@ -42,66 +33,36 @@
       }
     };
 
-    canvas = new fabric.Canvas(canv, {
-      isDrawingMode: true,
-    });
-
-    // canvas.on("mouse:down", () => {
-    //   isDrawing = true;
-    // });
-
-    // canvas.on("mouse:move", (options) => {
-    //   // // start capturing the data to send only after pointer is down
-    //   // if (isDrawing) {
-    //   //   console.log("Started drawing!");
-
-    //   //   // // all data to send
-    //   //   // posX = options.e.layerX;
-    //   //   // posY = options.e.layerY;
-    //   //   // const location = "drawingchallenge";
-    //   //   // const data = `{ "posX": ${posX}, "posY": ${posY}, "location": "${location}" }`;
-
-    //   //   // // send data
-    //   //   // ManageSession.socket.rpc("move_position", data);
-    //   // }
-    // });
-
+    // each mouse-up event sends the drawing
     canvas.on("mouse:up", () => {
-      // console.log("canvas.toJSON()", canvas.toJSON());
+      // get the drawing from the canvas in the format of SVG
       const canvasData = canvas.toSVG();
-      console.log("canvasData", canvasData);
 
+      // convert SVG into the HTML format in order to be able to manipulate inner data
       const parsedSVG = new DOMParser().parseFromString(
         canvasData,
         "text/html"
       );
 
-      console.log("parsedSVG", parsedSVG);
+      // all <g> tags contain drawing action
+      const gTagElement = parsedSVG.getElementsByTagName("g");
 
-      const tagElement = parsedSVG.getElementsByTagName("g");
-      console.log("tagElement", tagElement);
-
-      for (let i = 0; i < tagElement.length - 2; i++) {
-        tagElement[i].remove;
+      // loop through <g> tags, remove all previous drawings and leave only the last one
+      for (let i = 0; i < gTagElement.length - 2; i++) {
+        gTagElement[i].remove;
       }
 
-      console.log("parsedSVG", parsedSVG);
-
+      // needed SVG is stored inside of body which we want to send only
       const body = parsedSVG.getElementsByTagName("BODY")[0].innerHTML;
 
-      console.log("body", body);
-
       // all data to send
-
       const location = "drawingchallenge";
       const dataToSend = `{ "action": ${JSON.stringify(
         body
       )}, "location": "${location}" }`;
-      console.log("dataToSend", dataToSend);
-      // // send data
-      ManageSession.socket.rpc("move_position", dataToSend);
 
-      // isDrawing = false;
+      // send data
+      ManageSession.socket.rpc("move_position", dataToSend);
     });
   });
 </script>
