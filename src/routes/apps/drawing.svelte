75a5fe7a706fd2598963c5d260186344814755ba
atabek-bakh <coderonline@icloud.com>
2022-05-27 12:36:12 +0200

    css drawing

--- src/routes/apps/drawing.svelte
+++ src/routes/apps/drawing.svelte
@@ -1968,25 +1968,60 @@
     }
 
     .optionbar {
-      margin-left: 0;
+      margin: 0;
       border-right: none;
-
-      border-top: 2px solid #7300ed;
+      border-top: 2px dotted #7300ed;
       height: min-content;
-      background-color: white;
-      transition: all 0.5s ease-in-out;
+      width: initial;
       padding: 0px;
-      transform: translateY(0%);
-      /* float: right; */
-      width: 90vw;
+      transition: none;
+      animation: growup 0.3s ease-in-out forwards;
+      transform-origin: bottom center;
+      /* width: 90vw;
+      float: right;
+      position: relative;
+      z-index: 20;
+      border-left: 2px solid #7300ed; */
+    }
+
+    @keyframes growup {
+      0% {
+        transform: scaleY(0);
+      }
+      80% {
+        transform: scaleY(1.1);
+      }
+      100% {
+        transform: scaleY(1);
+      }
     }
 
     .optionbar.hidden {
-      width: 100vw;
-      transform: translateY(160%);
+      margin: 0;
+      border-right: none;
+      height: min-content;
+      width: auto;
+      transform: none;
       display: inline;
-      padding: 0px;
-      margin: 0px;
+      animation: growdown 2s ease-in-out forwards;
+    }
+
+    @keyframes growdown {
+      0% {
+        transform: scaleY(1);
+      }
+      50% {
+        transform: scaleY(0.5);
+      }
+      100% {
+        transform: scaleY(0);
+      }
+    }
+
+    .box2 {
+      position: fixed;
+      -ms-transform: initial;
+      transform: initial;
     }
 
     .currentSelected {
