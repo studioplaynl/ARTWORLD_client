588d7e77930bb252754622950fdd1e72bb9e593a
maarten <maartenvanderglas@gmail.com>
2022-05-30 14:55:02 +0200

    small topbar design change

--- src/routes/components/topbar.svelte
+++ src/routes/components/topbar.svelte
@@ -68,8 +68,8 @@
     }
 
     .TopIcon {
-        width: 50px;
-        height: 50px;
+        width: 40px;
+        height: 40px;
         border-radius: 50%;
         background-color: white;
     }
