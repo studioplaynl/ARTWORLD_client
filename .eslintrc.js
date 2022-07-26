c8716572de9709ccf1fd421a3481b3ff97a8f70a
Eelke Feenstra <dev@eelke.net>
2022-07-26 15:05:54 +0200

    Disable some Airbnb eslint-rules for svelte files
    Signed-off-by: Eelke Feenstra <dev@eelke.net>

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,20 @@ module.exports = {
     'linebreak-style': 0,
     'import/first': 0,
     'max-len': ['error', { code: 120 }],
+    'no-multiple-empty-lines': 'off',
+    'no-plusplus': 'off',
   },
   overrides: [ // this stays the same
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
+      rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'import/no-mutable-exports': 0,
+        'import/prefer-default-export': 0,
+        'no-labels': 0,
+        'no-restricted-syntax': 0,
+      },
     },
   ],
   settings: {
