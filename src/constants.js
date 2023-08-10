c36a0b2ecab45b120d2836b59eb3c7d14d67c28b
maarten <maartenvanderglas@gmail.com>
2023-08-10 16:05:08 +0200

    changes version number

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const CONFIG = {
 };
 
 /** version info: date time of push and branch name */
-export const APP_VERSION_INFO = '#2023-JUNE-8 14:55 DEVELOP#';
+export const APP_VERSION_INFO = '#2023-AUG-10 16:05 DEVELOP#';
 /** List of Scenes a user may navigate to */
 export const SCENE_INFO = [
   {
