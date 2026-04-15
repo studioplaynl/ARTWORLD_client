26a49897eca15412d0302cf249e5b2fc1c87412c
mrmaarten <maartenvanderglas@gmail.com>
2026-04-15 08:25:57 +0200

    refactor ESLint configuration to use eslint-plugin-svelte instead of eslint-plugin-svelte3; update package dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js';
-import svelte3 from 'eslint-plugin-svelte3';
+import svelte from 'eslint-plugin-svelte';
 import airbnbBase from 'eslint-config-airbnb-base';
 import importPlugin from 'eslint-plugin-import';
 import globals from 'globals';
@@ -8,17 +8,13 @@ const airbnbBaseConfig = airbnbBase;
 
 export default [
   js.configs.recommended,
+  ...svelte.configs['flat/recommended'],
   {
     files: ['*.js', '*.svelte'],
     ...airbnbBaseConfig,
   },
   {
-    files: ['*.svelte'],
-    processor: svelte3.processors.svelte3,
-  },
-  {
     plugins: {
-      svelte3,
       import: importPlugin,
     },
     rules: {
@@ -28,9 +24,6 @@ export default [
       'no-multiple-empty-lines': 'off',
       'no-plusplus': 'off',
       'no-unused-vars': 'warn',
-    },
-    settings: {
-      'svelte3/ignore-styles': () => true,
     },
     languageOptions: {
       globals: {
